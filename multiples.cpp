e73c6f51e5bb21674e8827b1bfad84cdaee9d46a
Justin Marcel Camonayan <124210943+optimusProgrammator@users.noreply.github.com>
2024-10-02 10:44:02 -0700

    Create multiples.cpp
    Given two numbers n and m, output the first m multiples of the number n.
    
    Input Format
    
    n m
    
    Constraints
    
    1 <= n <= 100
    
    1 <= m <= 50
    
    Output Format
    
    0*n 1*n 2*n 3*n .. m*n
    
    Sample Input 0
    
    3 5
    Sample Output 0
    
    0 3 6 9 12 15

---
+++ multiples.cpp
@@ -0,0 +1,40 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+int main() {
+    
+    int n;
+    int m;
+    int index;
+    
+    
+    cout << "";
+    cin >> n >> m;
+    
+    for (index = 0; index <= m; index++)
+    {
+        cout << index * n << " ";
+    }
+    return 0;
+}
